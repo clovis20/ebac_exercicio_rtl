8be76a45d6a868761b0ea553a06212e86bfc1390
clovis20 <36241818+clovis20@users.noreply.github.com>
2023-08-24 16:58:36 -0300

    Add files via upload

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,26 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment/>)
+
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target: {
+                value: '1* Comentário adicionado',
+            }
+        })
+
+        fireEvent.click(screen.getByTestId('comentario-btn'))
+
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target: {
+                value: '2* Comentário adicionado'
+            }
+        })
+
+        fireEvent.click(screen.getByTestId('comentario-btn'))
+
+        expect(screen.getAllByTestId('comentario-elem')).toHaveLength(2);
+    })
 });
